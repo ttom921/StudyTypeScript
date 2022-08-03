3c7042a4d21f37a3721cdc74fc79ebade7b72aa8
ttom921 <ttom921@hotmail.com>
2022-08-03 09:33:37 +0800

    Day15

--- typescript-tutorial/02-interface-class/index.js
+++ typescript-tutorial/02-interface-class/index.js
@@ -118,5 +118,93 @@ const implementAddition = {
       `);
     }
 };
-// 4. 其他讀者可能想得到的案例...
-//#endregion Day14
+;
+;
+let myDic = {
+    A: 'Key is A',
+    ['B']: 'Key is B',
+    1: 'Key is 1',
+    [2]: 'Key is 2'
+};
+console.log(myDic['A']);
+console.log(myDic.A);
+console.log(myDic['B']);
+console.log(myDic[1]);
+console.log(myDic[2]);
+// Dictionary 範例
+// 正常使用方式
+let normalDictionary = {
+    hello: 'world',
+    thisFeature: 'is crazy'
+};
+// 空的狹義物件狀態也行
+let emptyDictionary = {};
+// 錯誤的使用方式
+// let wrongDictionary: Dictionary = {
+//   hello: 123,
+//   thisFeature: true,
+//   withFunction() { console.log('Wrong type!'); },
+//   nestedDictionary: { hello: 123 },
+// };
+// StringTypedList 範例
+// 正常使用方式
+let stringTypedArray = {
+    123: 'Hello',
+    [456]: 'Hi',
+};
+// 空的狹義物件狀態也行
+let emptyStringTypedArray = {};
+// 但不可以直接變成 Array
+// let stringTypedArrayLiteral: StringTypedList = [1, 2, 3];
+// 但卻可以為空的 Array
+let emptyStringTypedArray2 = [];
+// 可以用數字進行索引
+stringTypedArray[0];
+stringTypedArray[1];
+let sampleAccount = {
+    email: 'maxwell@example.com',
+    password: '<hashed-password>',
+    name: 'Maxwell'
+};
+// 可以讀取
+sampleAccount.email;
+function createCounter() {
+    let value;
+    let initializedNumber;
+    // 實踐純函式的部分
+    const counter = (function (startNumber) {
+        initializedNumber = startNumber;
+        value = startNumber;
+    });
+    // 實踐狹義物件格式的部分
+    counter.increment = function () {
+        value++;
+        return value;
+    };
+    counter.reset = function () {
+        value = initializedNumber;
+    };
+    Object.defineProperty(counter, 'value', {
+        get() { return value; }
+    });
+    return counter;
+}
+// 建立一個 counter 物件
+const counter = createCounter();
+// 藉由 Counter 介面裡面中，純函式型別裡的格式：
+// (start: number): void
+// 我們可以填入數字
+counter(5); // <- 初始化值為 5
+// 呼叫 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 5
+// 呼叫 3 次 Counter 介面裡的 increment 方法
+counter.increment();
+counter.increment();
+counter.increment();
+// 再呼叫一次 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 8
+// 呼叫 Counter 介面裡的 reset 方法
+counter.reset();
+// 再呼叫一次 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 5，也就是原本的初始值
+//#endregion Day15
--- typescript-tutorial/02-interface-class/index.ts
+++ typescript-tutorial/02-interface-class/index.ts
@@ -292,61 +292,61 @@ interface IUserAccount extends IAccountSystem, IAccountPersonalInfo { }
 interface AddOperation {
     addition(p1: number, p2: number): number;
     addition(p1: string, p2: string): number;
-  }
-  
-  const implementAddition: AddOperation = {
+}
+
+const implementAddition: AddOperation = {
     addition(p1: number | string, p2: number | string) {
-      if (typeof p1 === 'number' && typeof p2 === 'number') {
-        return p1 + p2;
-      } else if (typeof p1 === 'string' && typeof p2 === 'string') {
-        return parseInt(p1, 10) + parseInt(p2, 10);
-      }
-  
-      throw new Error(`
+        if (typeof p1 === 'number' && typeof p2 === 'number') {
+            return p1 + p2;
+        } else if (typeof p1 === 'string' && typeof p2 === 'string') {
+            return parseInt(p1, 10) + parseInt(p2, 10);
+        }
+
+        throw new Error(`
         Parameter \`p1\` and \`p2\` should only accept both \`number\`
         type or \`string\` type.
       `);
     }
-  };
-  
-  /* 其他案例讀者試試看 */
-  // 1. 參數與回傳型別一模一樣
-  interface AddOperation1 {
+};
+
+/* 其他案例讀者試試看 */
+// 1. 參數與回傳型別一模一樣
+interface AddOperation1 {
     add(p1: number, p2: number): number;
     add(p1: number, p2: number): number;
-  }
-  
-  // 2. 回傳型別不同但是參數相同
-  interface AddOperation2 {
+}
+
+// 2. 回傳型別不同但是參數相同
+interface AddOperation2 {
     add(p1: number, p2: number): number;
     add(p1: number, p2: number): string;
-  }
-  
-  // 3. 參數數量不同
-  interface AddOperation2 {
+}
+
+// 3. 參數數量不同
+interface AddOperation2 {
     add(p1: number): number;
     add(p1: number, p2: number): number;
-  }
-  
-  // 4. 其他讀者可能想得到的案例...
-  
-  /* 介面融合 Interface Merging */
+}
+
+// 4. 其他讀者可能想得到的案例...
+
+/* 介面融合 Interface Merging */
 // Block-Level Elements
 interface MyDocument {
-    createElement(tag: 'p'):    HTMLParagraphElement;
+    createElement(tag: 'p'): HTMLParagraphElement;
     createElement(tag: 'body'): HTMLBodyElement;
-    createElement(tag: 'div'):  HTMLDivElement;
-  }
-  
-  // Inline-Level Elements
-  interface MyDocument {
-    createElement(tag: 'a'):     HTMLAnchorElement;
-    createElement(tag: 'span'):  HTMLSpanElement;
+    createElement(tag: 'div'): HTMLDivElement;
+}
+
+// Inline-Level Elements
+interface MyDocument {
+    createElement(tag: 'a'): HTMLAnchorElement;
+    createElement(tag: 'span'): HTMLSpanElement;
     createElement(tag: 'input'): HTMLInputElement;
-  }
-  
-  
-  // 第三方套件的 Definition File：
+}
+
+
+// 第三方套件的 Definition File：
 //   namespace StupidFramework {
 //     interface StupidRequest {
 //       headers: Header[];
@@ -356,16 +356,197 @@ interface MyDocument {
 //       ...
 //     }  
 //   }
-  
-  /* -------- 分隔線代表不同的檔案 -------- */
-  
-  // 我們的專案自定義的狀態
+
+/* -------- 分隔線代表不同的檔案 -------- */
+
+// 我們的專案自定義的狀態
 //    type Dictionary = { [propName: string]: string };
-  
+
 //   namespace StupidFramework {
 //     interface StupidRequest {
 //       query?: Dictionary;
 //     }
 //   }
-  
+
 //#endregion Day14
+//#region Day15
+/* Indexable Types */
+// 定義一種物件型別 Dictionary，其中，它的鍵值對都是字串型態
+type Dictionary = {
+    [propName: string]: string,
+};
+
+// 定義類似陣列的型別，其裡面儲存的值為字串
+interface StringTypedList {
+    [index: number]: string;
+};
+interface IDictionary {
+    [propName: string | number]: string,
+};
+let myDic: IDictionary = {
+    A: 'Key is A',
+    ['B']: 'Key is B',
+    1: 'Key is 1',
+    [2]: 'Key is 2'
+};
+// console.log(myDic['A']);
+// console.log(myDic.A);
+// console.log(myDic['B']);
+// console.log(myDic[1]);
+// console.log(myDic[2]);
+// Dictionary 範例
+// 正常使用方式
+let normalDictionary: Dictionary = {
+    hello: 'world',
+    thisFeature: 'is crazy'
+};
+
+// 空的狹義物件狀態也行
+let emptyDictionary: Dictionary = {};
+
+// 錯誤的使用方式
+// let wrongDictionary: Dictionary = {
+//   hello: 123,
+//   thisFeature: true,
+//   withFunction() { console.log('Wrong type!'); },
+//   nestedDictionary: { hello: 123 },
+// };
+
+// StringTypedList 範例
+// 正常使用方式
+let stringTypedArray: StringTypedList = {
+    123: 'Hello',
+    [456]: 'Hi',
+};
+
+// 空的狹義物件狀態也行
+let emptyStringTypedArray: StringTypedList = {};
+
+// 但不可以直接變成 Array
+// let stringTypedArrayLiteral: StringTypedList = [1, 2, 3];
+
+// 但卻可以為空的 Array
+let emptyStringTypedArray2: StringTypedList = [];
+
+// 可以用數字進行索引
+stringTypedArray[0];
+stringTypedArray[1];
+
+// 錯誤使用方式
+// let wrongStringTypedArray: StringTypedList = {
+//   message: 'Hello',
+//   thisFeature: true
+// };
+
+// 也當然不能被字串索引
+// stringTypedArray['hello'];
+
+// 當然更不可能用點來呼叫屬性，因為會被當成字串
+// stringTypedArray.hello;
+
+/* 選用屬性 Optional Properties */
+type TAccountUser = {
+    email: string,
+    password: string,
+    name?: string,
+    age?: number,
+};
+
+interface IAccountUser {
+    email: string;
+    password: string;
+    name?: string;
+    age?: number;
+}
+
+/* 唯讀屬性 */
+type TAccountUserWithReadonlyProperty = {
+    readonly email: string,
+    readonly password: string,
+    name?: string,
+    age?: number,
+};
+
+interface IAccountUserWithReadonlyProperty {
+    readonly email: string;
+    readonly password: string;
+    name?: string;
+    age?: number;
+}
+
+let sampleAccount: TAccountUserWithReadonlyProperty = {
+    email: 'maxwell@example.com',
+    password: '<hashed-password>',
+    name: 'Maxwell'
+};
+
+// 可以讀取
+sampleAccount.email;
+
+// 但不能寫入 => 強行寫入就被 TS 警告
+// sampleAccount.email = 'new@example.com';
+
+/* 混合型介面 Hybrid Type Interface */
+interface Counter {
+    // 純函式格式
+    (start: number): void;
+
+    // 狹義物件的格式
+    increment(): number;
+    reset(): void;
+    value: number;
+}
+
+function createCounter(): Counter {
+    let value: number;
+    let initializedNumber: number;
+
+    // 實踐純函式的部分
+    const counter = (function (startNumber: number) {
+        initializedNumber = startNumber;
+        value = startNumber;
+    }) as Counter;
+
+    // 實踐狹義物件格式的部分
+    counter.increment = function () {
+        value++;
+        return value;
+    };
+
+    counter.reset = function () {
+        value = initializedNumber;
+    };
+
+    Object.defineProperty(counter, 'value', {
+        get() { return value; }
+    });
+
+    return counter;
+}
+
+// 建立一個 counter 物件
+const counter: Counter = createCounter();
+
+// 藉由 Counter 介面裡面中，純函式型別裡的格式：
+// (start: number): void
+// 我們可以填入數字
+counter(5); // <- 初始化值為 5
+
+// 呼叫 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 5
+
+// 呼叫 3 次 Counter 介面裡的 increment 方法
+counter.increment();
+counter.increment();
+counter.increment();
+
+// 再呼叫一次 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 8
+
+// 呼叫 Counter 介面裡的 reset 方法
+counter.reset();
+
+// 再呼叫一次 Counter 介面裡的 value 屬性
+//console.log(counter.value); // 應該要得出 5，也就是原本的初始值
+
+//#endregion Day15
