e6467efebf9cd48560f5f10e76a3f9198d97bec5
ttom921 <ttom921@hotmail.com>
2022-08-03 09:05:58 +0800

    Day14

--- typescript-tutorial/02-interface-class/index.js
+++ typescript-tutorial/02-interface-class/index.js
@@ -104,8 +104,19 @@ let duckIsLiterallyDuck = {
     noise: 'Quack~quack~quack~quack~quack~',
     makeNoise() { console.log('Quack!'); },
 };
-pokeTheDuck(maxwellCanBeDuck);
-pokeTheDuck(kittyCanBeDuck);
-pokeTheDuck(vehicleCanBeDuck);
-pokeTheDuck(duckIsLiterallyDuck);
-//#endregion Day13
+const implementAddition = {
+    addition(p1, p2) {
+        if (typeof p1 === 'number' && typeof p2 === 'number') {
+            return p1 + p2;
+        }
+        else if (typeof p1 === 'string' && typeof p2 === 'string') {
+            return parseInt(p1, 10) + parseInt(p2, 10);
+        }
+        throw new Error(`
+        Parameter \`p1\` and \`p2\` should only accept both \`number\`
+        type or \`string\` type.
+      `);
+    }
+};
+// 4. 其他讀者可能想得到的案例...
+//#endregion Day14
