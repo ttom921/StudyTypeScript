cd3f0a4847bb51f760d21ef8e298155f582a9c06
ttom921 <ttom921@hotmail.com>
2022-08-12 09:09:37 +0800

    Day19

--- typescript-tutorial/03-interface-class/index.js
+++ typescript-tutorial/03-interface-class/index.js
@@ -76,13 +76,77 @@ let customInfo1 = new CustomPersonInfo();
 // customInfo1.printInfo();
 // 2. 填入自訂的參數
 let customInfo2 = new CustomPersonInfo('Toby', 32, true);
-console.log(customInfo2);
-customInfo2.printInfo();
-// 填錯型別
-// let wrongCustomInfo = new CustomPersonInfo('Toby', '32', true);
-// let correctCustomInfo = new CustomPersonInfo('Leo', 28, false);
-// 呼叫不存在的屬性
-// correctCustomInfo.inexistedProps;
-// 呼叫不存在的方法
-// correctCustomInfo.inexistedMethod();
-//#endregion Day18
+// 實踐ICashMachine介面
+class CashMachine {
+    constructor() {
+        //假設我們這些使用者
+        this.users = [
+            { account: 'Maxwell', password: '123', money: 12345 },
+            { account: 'Martin', password: '456', money: 54321 },
+            { account: 'Chairman Guo.', password: '789', money: 1000000000 },
+        ];
+    }
+    signIn(account, password) {
+        // 因為 Array.prototype.find 是 ES6 語法，暫時先用 ES5 的方式處理
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i];
+            if (user.account === account &&
+                user.password === password) {
+                this.currentUser = user;
+                break;
+            }
+            if (this.currentUser === undefined) {
+                throw new Error("User not foune");
+            }
+        }
+    }
+    signOut() {
+        // 清除目前的使用者
+        this.currentUser = undefined;
+    }
+    deposit(amount) {
+        if (this.currentUser !== undefined) {
+            this.currentUser.money += amount;
+        }
+        else {
+            throw new Error('No user signed in!');
+        }
+    }
+    withdraw(amount) {
+        if (this.currentUser !== undefined) {
+            this.currentUser.money -= amount;
+        }
+        else {
+            throw new Error('No user signed in!');
+        }
+    }
+}
+;
+function printAccountInfo(input) {
+    if (input === undefined) {
+        console.log('No user found!');
+    }
+    else {
+        console.log(`
+        Current User: ${input.account}
+        Money: ${input.money}
+      `);
+    }
+}
+// 初始化新的提款機
+const machine = new CashMachine();
+console.log('Initialized: ');
+printAccountInfo(machine.currentUser); //<-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+// 登入過後
+machine.signIn('Maxwell', '123');
+console.log('Signed In: ');
+printAccountInfo(machine.currentUser); // <-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+// 提款 5000 過後
+machine.withdraw(5000);
+console.log('After Withdrawal: ');
+printAccountInfo(machine.currentUser); // <-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+// 登出過後
+machine.signOut();
+console.log('Signed Out: ');
+printAccountInfo(machine.currentUser); //<-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+//#endregion Day19
--- typescript-tutorial/03-interface-class/index.ts
+++ typescript-tutorial/03-interface-class/index.ts
@@ -113,3 +113,128 @@ let customInfo2 = new CustomPersonInfo('Toby', 32, true);
 
 
 //#endregion Day18
+//#region Day19
+/* 提款機範例 */
+// 定義每一個使用陽春的提款機的用戶的資訊
+type TUserAccount = {
+    account: string;
+    password: string;
+    money: number;
+};
+// // 定義陽春的提款機介面
+// interface ICashMachine {
+//     // 存錢錢，裡面填入要存會的量
+//     deposit(amount: number): void;
+//     //提領錢錢，裡面填入要提錢的量
+//     withdraw(amonut: number): void;
+//     // 一系列的使用帳戶
+//     users: TUserAccount[];
+//     // 登入的使用者，可為空代表還沒登入
+//     currentUser: TUserAccount | undefined;
+//     // 登入系統，必須填入帳戶與密碼
+//     signIn(account: string, passwor: string): void;
+//     // 登出系統
+//     signOut(): void;
+// }
+
+// 定義陽春的提款機介面的帳戶管理系統
+interface AccountSystem {
+    // 登入系統，必須填入帳戶與密碼
+    signIn(account: string, password: string): void;
+
+    // 登出系統
+    signOut(): void;
+}
+// 定義陽春的提款機介面的交易系統
+interface TransactionSystem {
+    // 存錢錢，裡面填入要存錢的量
+    deposit(amount: number): void;
+
+    // 提領錢錢，裡面填入要提錢的量
+    withdraw(amount: number): void;
+}
+
+// 定義陽春的提款機介面的完整系統
+interface ICashMachine extends TransactionSystem, AccountSystem { }
+
+// 實踐ICashMachine介面
+class CashMachine implements ICashMachine {
+    //假設我們這些使用者
+    private users: TUserAccount[] = [
+        { account: 'Maxwell', password: '123', money: 12345 },
+        { account: 'Martin', password: '456', money: 54321 },
+        { account: 'Chairman Guo.', password: '789', money: 1000000000 },
+    ];
+    private currentUser: TUserAccount | undefined;
+
+    signIn(account: string, password: string): void {
+        // 因為 Array.prototype.find 是 ES6 語法，暫時先用 ES5 的方式處理
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i];
+            if (
+                user.account === account &&
+                user.password === password
+            ) {
+                this.currentUser = user;
+                break;
+            }
+            if (this.currentUser === undefined) {
+                throw new Error("User not foune");
+
+            }
+        }
+    }
+    signOut() {
+        // 清除目前的使用者
+        this.currentUser = undefined;
+    }
+    deposit(amount: number) {
+        if (this.currentUser !== undefined) {
+            this.currentUser.money += amount;
+        } else {
+            throw new Error('No user signed in!');
+        }
+    }
+
+    withdraw(amount: number) {
+        if (this.currentUser !== undefined) {
+            this.currentUser.money -= amount;
+        } else {
+            throw new Error('No user signed in!');
+        }
+    }
+
+
+};
+function printAccountInfo(input: TUserAccount | undefined) {
+    if (input === undefined) {
+        console.log('No user found!');
+    } else {
+        console.log(`
+        Current User: ${input.account}
+        Money: ${input.money}
+      `);
+    }
+}
+
+// 初始化新的提款機
+const machine = new CashMachine();
+//console.log('Initialized: ');
+//printAccountInfo(machine.currentUser); //<-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+
+// 登入過後
+machine.signIn('Maxwell', '123');
+//console.log('Signed In: ');
+//printAccountInfo(machine.currentUser);// <-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+
+// 提款 5000 過後
+machine.withdraw(5000);
+//console.log('After Withdrawal: ');
+//printAccountInfo(machine.currentUser);// <-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+
+// 登出過後
+machine.signOut();
+//console.log('Signed Out: ');
+//printAccountInfo(machine.currentUser); //<-- 因為 currentUser 變成 private 模式，不能在外面被呼叫！
+
+//#endregion Day19
