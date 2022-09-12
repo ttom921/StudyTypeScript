f3855d2904435a11cafc0350920af0805ff66413
ttom921 <ttom921@hotmail.com>
2022-09-12 10:19:47 +0800

    Day 30. 機動藍圖・流言終結者 X 重新認識物件的複合 - Favour Object Composition Over Class Inheritance

--- typescript-tutorial/03-interface-class/index.ts
+++ typescript-tutorial/03-interface-class/index.ts
@@ -1051,22 +1051,22 @@ const wantedMonster = new Monster('Eikthyrnir');
 const desperado = new Character('Legendary Joe', Role.Highwayman);
 
 // 百分之百可以抓到普通角色
-bountyHunter.capture(wantedCharacter, 1);
+//bountyHunter.capture(wantedCharacter, 1);
 
 // 可能可以抓到怪物
-bountyHunter.capture(wantedMonster, 0.5);
+//bountyHunter.capture(wantedMonster, 0.5);
 
 // 不太可能抓到亡命之徒
-bountyHunter.capture(desperado, 0.01);
+//bountyHunter.capture(desperado, 0.01);
 
 // 賣掉人質
-bountyHunter.sellHostages();
+//bountyHunter.sellHostages();
 
 // 普通角色是否能夠回擊呢？
-wantedCharacter.attack(bountyHunter);
+//wantedCharacter.attack(bountyHunter);
 
 // 怪物是否也能夠回擊呢？
-wantedMonster.attack(bountyHunter);
+//wantedMonster.attack(bountyHunter);
 
 // 如果被強行註記為 ICharacter 的變數，是否能夠接受 BountyHunter 類型的值？
 const anyCharacter: ICharacter = new BountyHunter('Alexius');
@@ -1075,3 +1075,47 @@ const anyCharacter: ICharacter = new BountyHunter('Alexius');
 //anyCharacter.capture(wantedMonster, 0.5);
 
 //#endregion Day25
+//#region Day30
+
+interface Geometry {
+    area(): number;
+    circumference(): number;
+}
+
+class Rectangle implements Geometry {
+    constructor(public width: number, public height: number) { }
+    public area() { return this.width * this.height }
+    public circumference() { return 2 * (this.width + this.height); }
+}
+
+class Circle implements Geometry {
+    public static PI = 3.14;
+    constructor(public radius: number) { }
+    public area() { return Circle.PI * (this.radius ** 2) }
+    public circumference() { return 2 * Circle.PI * this.radius; }
+}
+//建立一個視窗物件
+class MyWindow {
+    //設置一個參考點對Geometry 相關物件進行委任的動作
+    constructor(public geometry: Geometry) { }
+    //計算面積，遞給委任的物件執行
+    public area() {
+        return this.geometry.area();
+    }
+    //計算周長時，遞給委任的物件執行
+    public circumference() {
+        return this.geometry.circumference();
+    }
+}
+
+//建立長方形視窗物件
+let rectWindow = new MyWindow(new Rectangle(50, 100));
+console.log(`Area of rectangular window:         ${rectWindow.area()}`);
+console.log(`Circumference of rectangular window ${rectWindow.circumference()}`);
+
+//建立一個圓形視窗物件
+let circularWindow = new MyWindow(new Circle(10));
+console.log(`Area of circular window:         ${circularWindow.area()}`);
+console.log(`Circumference of circular window ${circularWindow.circumference()}`);
+
+//#endregion Day30
