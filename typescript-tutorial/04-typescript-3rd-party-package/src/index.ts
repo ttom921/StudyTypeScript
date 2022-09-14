e12caa5070e743bbdfb1d30dc4a8b3098600d001
ttom921 <ttom921@hotmail.com>
2022-09-14 11:37:01 +0800

    Day 33 TypeScript Compiler Debug Techniques

---
+++ typescript-tutorial/04-typescript-3rd-party-package/src/index.ts
@@ -0,0 +1,20 @@
+// 取得按鈕元素
+const $btn = document.getElementById('click-me-btn');
+
+// 取得計睥的span 元素
+const $counter = document.getElementById('count');
+//使用 TYpe Guard
+if ($btn === null || $counter === null) {
+    throw new Error("Elemants shouldn\'t be null");
+}
+
+//累計按鈕次數
+let count = 0;
+
+//每一次按鈕被按到就會將計數次數上升
+$btn.addEventListener('click', () => {
+    count++;
+    console.log("You clicked me!");
+
+    $counter.innerText = count.toString();
+});
