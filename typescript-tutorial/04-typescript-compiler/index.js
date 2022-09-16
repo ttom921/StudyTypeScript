15e7d3f1ed31fdf46f4ffcce431238afccfb99ce
ttom921 <ttom921@hotmail.com>
2022-09-16 09:59:43 +0800

    Day 35 TypeScript Namespaces Introduction

--- typescript-tutorial/04-typescript-compiler/index.js
+++ typescript-tutorial/04-typescript-compiler/index.js
@@ -4,6 +4,35 @@
 // //指派a到b
 // const b = a;
 // 宣告名為summation 的函式
-function summation(p1, p2) {
-    return p1 + p2;
-}
+// function summation(p1, p2) {
+//     return p1 + p2;
+// }
+/* 宣告名為 MyMath 的命名空間*/
+var MyMath;
+(function (MyMath) {
+    // 圓周率
+    MyMath.PI = 3.14;
+    //計算圓的面積
+    function AreaOfCircle(radius) {
+        return MyMath.PI * radius ** 2;
+    }
+    MyMath.AreaOfCircle = AreaOfCircle;
+    //計算長方形的面積
+    function AreaOfRectangel(width, height) {
+        return width * height;
+    }
+    MyMath.AreaOfRectangel = AreaOfRectangel;
+    //計算圓的周長
+    function CircumferenceOfCircle(radius) {
+        return 2 * MyMath.PI * radius;
+    }
+    MyMath.CircumferenceOfCircle = CircumferenceOfCircle;
+    // 計算長方形的周長
+    function CircumferenceOfRectangel(width, height) {
+        return 2 * (width + height);
+    }
+    MyMath.CircumferenceOfRectangel = CircumferenceOfRectangel;
+})(MyMath || (MyMath = {})); //namespace MyMath
+console.log(MyMath.PI);
+console.log(MyMath.AreaOfCircle(100));
+console.log(MyMath.CircumferenceOfRectangel(50, 100));
--- typescript-tutorial/04-typescript-compiler/index.ts
+++ typescript-tutorial/04-typescript-compiler/index.ts
@@ -5,6 +5,35 @@
 
 
 // 宣告名為summation 的函式
-function summation(p1, p2) {
-    return p1 + p2;
-}
+// function summation(p1, p2) {
+//     return p1 + p2;
+// }
+
+/* 宣告名為 MyMath 的命名空間*/
+namespace MyMath {
+    // 圓周率
+    export const PI = 3.14;
+
+    //計算圓的面積
+    export function AreaOfCircle(radius: number) {
+        return PI * radius ** 2;
+    }
+
+    //計算長方形的面積
+    export function AreaOfRectangel(width: number, height: number) {
+        return width * height;
+    }
+
+    //計算圓的周長
+    export function CircumferenceOfCircle(radius: number) {
+        return 2 * PI * radius;
+    }
+    // 計算長方形的周長
+    export function CircumferenceOfRectangel(width: number, height: number) {
+        return 2 * (width + height);
+    }
+}//namespace MyMath
+
+// console.log(MyMath.PI);
+// console.log(MyMath.AreaOfCircle(100));
+// console.log(MyMath.CircumferenceOfRectangel(50, 100));
