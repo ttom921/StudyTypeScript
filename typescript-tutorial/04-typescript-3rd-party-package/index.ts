7f2b17fc1b4f85fed791616ea90740ab054d441d
ttom921 <ttom921@hotmail.com>
2022-09-20 09:48:52 +0800

    Day 37-part1 第三方套件 X 支援的引入 - 3rd-Party Package & TypeScript Declaration File

---
+++ typescript-tutorial/04-typescript-3rd-party-package/index.ts
@@ -0,0 +1,12 @@
+//當網站的元素載入時執行
+
+$(document).ready(function () {
+    const $btn = $('#main-btn');
+    const $count = $('#count');
+    let count = 0;
+    //每一次按鈕被按的時候，更新$count元素
+    $btn.click(() => {
+        count++;
+        $count.text(count);
+    });
+});
