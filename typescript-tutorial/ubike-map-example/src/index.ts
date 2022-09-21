20be3079d82f305fe8a09de9b06dec42b8f167d3
ttom921 <ttom921@hotmail.com>
2022-09-21 10:36:16 +0800

    Day 38 UBike 地圖 X Webpack 環境建構 - TypeScript Webpack Integration

--- typescript-tutorial/ubike-map-example/src/index.ts
+++ typescript-tutorial/ubike-map-example/src/index.ts
@@ -2,17 +2,20 @@
 
 import L, { LatLngExpression } from 'leaflet';
 
-//台北市的座標
-const taipeiCoord: LatLngExpression = [25.0330, 121.5654];
+import mapConfig from './map.config';
 
-//地圖預設的縮放等級
-const zoom = 13;
+const {
+    coordinate,
+    zoomLevel,
+    tileLayerURL,
+    containerID,
+} = mapConfig;
 
 // 建立Leaflet 地圖個體,'map' 代表選擇ID為'map'的HTML元素
-const map = L.map('map');
+const map = L.map(containerID);
 
 //設定地圖要聚焦的座標與縮放等級
-map.setView(taipeiCoord, zoom);
+map.setView(coordinate, zoomLevel);
 
 //設定地圖的底圖為加到地圖的個體中
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer(tileLayerURL).addTo(map);
---
+++ typescript-tutorial/ubike-map-example/src/map.config.ts
@@ -0,0 +1,18 @@
+import { LatLngExpression } from 'leaflet';
+// ./src/map.config.ts
+
+//使用型別化名
+type MapConfig = {
+    coordinate: LatLngExpression,
+    zoomLevel: number,
+    tileLayerURL: string,
+    containerID: string,
+};
+
+//積極註記為MapConfig
+export default {
+    coordinate: [25.0330, 121.5654],
+    zoomLevel: 13,
+    tileLayerURL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    containerID: 'map',
+} as MapConfig;
