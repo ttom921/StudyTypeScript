34ccb4369ae8dc02b5a55398bb31d0a351a3561d
ttom921 <ttom921@hotmail.com>
2022-09-23 11:40:28 +0800

    Day 40 part2 — UBike 地圖 X 使用 LeafletJS - Using LeafletJS with TypeScript

--- typescript-tutorial/ubike-map-example/src/index.ts
+++ typescript-tutorial/ubike-map-example/src/index.ts
@@ -7,6 +7,7 @@ import fetchData from './fetchData';
 
 
 import mapConfig from './map.config';
+import { Districts } from './data';
 
 
 /**將所有的行政區 Options的HTML DOM建立起來 */
@@ -48,33 +49,49 @@ L.tileLayer(tileLayerURL).addTo(map);
 let markerLayer: LayerGroup;
 
 //取出目前的行政區
-let currentDistrict = $selectDistrict?.value;
+let currentDistrict = $selectDistrict?.value as Districts;
 
-/**繪製點卷 */
-fetchData().then(data => {
-    // UBike 資料轉換成點位的邏輯
-    //1. 生將資料根據選到的行政區進行過濾的動作
-    const selectData = data.filter(info => info.regionName === currentDistrict);
-    // 2. 將selectData裡面的UBikeInfo轉換成Leaflet Marker
-    const markers = selectData.map(data => {
-        //使用L.Marker 裡面填入 LatLngExpression
-        const marker = new L.Marker(data.latLng);
-        //顯示UBike的資料
-        marker.bindTooltip(`
+function updateUBikeMap(district: Districts) {
+    /**繪製點位 */
+    fetchData().then(data => {
+        // UBike 資料轉換成點位的邏輯
+        //1. 生將資料根據選到的行政區進行過濾的動作
+        const selectData = data.filter(info => info.regionName === district);
+        // 2. 將selectData裡面的UBikeInfo轉換成Leaflet Marker
+        const markers = selectData.map(data => {
+            //使用L.Marker 裡面填入 LatLngExpression
+            const marker = new L.Marker(data.latLng);
+            //顯示UBike的資料
+            marker.bindTooltip(`
         <p>${data.regionName} - ${data.stopName}</p>
         <p>總自行車數:${data.totalBikes}</p>
         <p>可用自行車數:${data.availableBikes}</p>
         `);
-        marker.on('mouseover', () => {
-            marker.openTooltip();
-        });
-        marker.on('mouserleave', () => {
-            marker.closeTooltip();
-        });
+            marker.on('mouseover', () => {
+                marker.openTooltip();
+            });
+            marker.on('mouserleave', () => {
+                marker.closeTooltip();
+            });
 
-        return marker;
+            return marker;
+        });
+        //3. 將所有的Marker丟進LayerGroup並新增到地圖裡
+        markerLayer = L.layerGroup(markers);
+        markerLayer.addTo(map);
     });
-    //3. 將所有的Marker丟進LayerGroup並新增到地圖裡
-    markerLayer = L.layerGroup(markers);
-    markerLayer.addTo(map);
+}
+updateUBikeMap(currentDistrict);
+/**不同的行政區應該要渲染出不同的狀況 */
+$selectDistrict?.addEventListener('change', (event) => {
+    //當行政區更變時，需要更新UBike地圖資訊
+    // 1.取得行政區的值
+    let { value } = event.target as HTMLSelectElement;
+    currentDistrict = value as Districts;
+
+    //2.將原本的Marker除掉
+    markerLayer.remove();
+    //3.更新地圖
+    updateUBikeMap(currentDistrict);
 });
+
