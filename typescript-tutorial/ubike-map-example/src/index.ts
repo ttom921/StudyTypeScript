46ecf823f3b5461f9e7c75299ac55ed7edf076de
ttom921 <ttom921@hotmail.com>
2022-09-23 11:31:37 +0800

    Day 40 part1 UBike 地圖 X 使用 LeafletJS - Using LeafletJS with TypeScript

--- typescript-tutorial/ubike-map-example/src/index.ts
+++ typescript-tutorial/ubike-map-example/src/index.ts
@@ -1,12 +1,32 @@
+import { distircts } from './districtData';
 //src/index.ts
 
-import L, { LatLngExpression } from 'leaflet';
+import L, { LatLngExpression, LayerGroup } from 'leaflet';
 
 import fetchData from './fetchData';
 
 
 import mapConfig from './map.config';
 
+
+/**將所有的行政區 Options的HTML DOM建立起來 */
+//選取<select> 標籤
+const $selectDistrict = <HTMLSelectElement | null>document.getElementById("select-district");
+//遍歷所有的行政區
+distircts.forEach((d) => {
+    //建立<option> 標籤
+    const $optionTag = document.createElement('option');
+    //將option標籤上value與內容
+    $optionTag.setAttribute('value', d);
+    $optionTag.innerText = d;
+    //使用Type Guard 排除掉$selectDistrict為null的情形 
+    if ($selectDistrict == null) {
+        return new Error("No selet-district field provided.");
+    }
+    $selectDistrict.appendChild($optionTag);
+});
+
+//以下是建立地圖
 const {
     coordinate,
     zoomLevel,
@@ -23,8 +43,38 @@ map.setView(coordinate, zoomLevel);
 //設定地圖的底圖為加到地圖的個體中
 L.tileLayer(tileLayerURL).addTo(map);
 
-//使用fetchData,由於fetch函式回傳的是Promise
-//因此必須接上.then來印出資料
+
+//新增 map layer 負責渲染所有的marker
+let markerLayer: LayerGroup;
+
+//取出目前的行政區
+let currentDistrict = $selectDistrict?.value;
+
+/**繪製點卷 */
 fetchData().then(data => {
-    console.log(data);
+    // UBike 資料轉換成點位的邏輯
+    //1. 生將資料根據選到的行政區進行過濾的動作
+    const selectData = data.filter(info => info.regionName === currentDistrict);
+    // 2. 將selectData裡面的UBikeInfo轉換成Leaflet Marker
+    const markers = selectData.map(data => {
+        //使用L.Marker 裡面填入 LatLngExpression
+        const marker = new L.Marker(data.latLng);
+        //顯示UBike的資料
+        marker.bindTooltip(`
+        <p>${data.regionName} - ${data.stopName}</p>
+        <p>總自行車數:${data.totalBikes}</p>
+        <p>可用自行車數:${data.availableBikes}</p>
+        `);
+        marker.on('mouseover', () => {
+            marker.openTooltip();
+        });
+        marker.on('mouserleave', () => {
+            marker.closeTooltip();
+        });
+
+        return marker;
+    });
+    //3. 將所有的Marker丟進LayerGroup並新增到地圖裡
+    markerLayer = L.layerGroup(markers);
+    markerLayer.addTo(map);
 });
