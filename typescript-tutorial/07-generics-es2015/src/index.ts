a69d043b9cdb835c606e308e552ce1dc1598283c
ttom921 <ttom921@hotmail.com>
2022-10-12 09:57:23 +0800

    Day 50 TypeScript Generics with Asynchronous Programming III. Async Functions

--- typescript-tutorial/07-generics-es2015/src/index.ts
+++ typescript-tutorial/07-generics-es2015/src/index.ts
@@ -272,3 +272,289 @@ function* positiveNumberIterator() {
     }
 }
 //#endregion Day 49
+//#region Day 50
+type PongResponse = {
+    data: string | null,
+    status: 200 | 500,
+};
+// pingRequest 會回傳 pong 訊息作為 response
+function pingRequest(
+    num: number,
+    errprProbability: number = 0,
+): Promise<PongResponse> {
+    return new Promise<PongResponse>((resolve, reject) => {
+        const probability = Math.random();
+        if (probability < errprProbability) {
+            reject({ data: null, status: 500 });
+        }
+
+        const timeout = Math.random() * 3000;
+
+        setTimeout(() => {
+            resolve({ data: `Pong ${num}`, status: 200 });
+        }, timeout);
+    });
+}
+/** Promise Chain 表示方式 */
+// console.time('1st Pong');
+// console.time('2nd Pong');
+// console.time('3rd Pong');
+
+// console.log('Promise Chain Respresentation:');
+// pingRequest(1)
+//     .then(response => {
+//         console.log(response.data);
+//         console.timeEnd('1st Pong');
+//         return pingRequest(2);
+//     })
+//     .then(response => {
+//         console.log(response.data);
+//         console.timeEnd('2nd Pong');
+//         return pingRequest(3);
+//     })
+//     .then(response => {
+//         console.log(response.data);
+//         console.timeEnd('3rd Pong');
+//     })
+//     .catch(response => {
+//         console.log(`Status: $${response.status}`);
+//     });
+
+/** Generator 表示形式 */
+// console.time('1st Pong');
+// console.time('2nd Pong');
+// console.time('3rd Pong');
+
+// function* pingsGenerator(): any {
+//     const response1 = yield pingRequest(1);
+//     console.log(response1.data);
+//     console.timeEnd('1st Pong');
+
+//     const response2 = yield pingRequest(2);
+//     console.log(response2.data);
+//     console.timeEnd('2nd Pong');
+
+//     const response3 = yield pingRequest(3);
+//     console.log(response3.data);
+//     console.timeEnd('3rd Pong');
+// }
+
+// // 建立一個 pingrequest 的迭代器
+// let pingsIter = pingsGenerator();
+
+// // 縮寫的命名代表 Pong-Response-Promise 物件
+// type PRP = Promise<PongResponse>;
+// // 第一輪迭代 --啟動迭代器
+// (pingsIter.next().value as PRP).then(respose1 => {
+//     console.log('Response 1 received');
+//     // 將response1 傳入 next 並進行第二輪迭代
+//     (pingsIter.next(respose1).value as PRP).then(respose2 => {
+//         console.log('Response 2 received');
+//         // 將response2 傳入 next 並進行第三輪迭代
+//         (pingsIter.next(respose2).value as PRP).then(response3 => {
+//             console.log('Response 3 received');
+//             // 將response3 傳入 next,並進行最後一輪迭代
+//             pingsIter.next(response3);
+//         })
+//     })
+// })
+
+/**宣告專門接收 generator 為參數的函式 */
+// function runGenerator(gen: () => Generator) {
+//     const iter = gen();
+
+//     //使用遞迴技巧去處理 request-response 過程的邏輯
+//     function recursiveIteration(pushResponse: any) {
+//         const result = iter.next(pushResponse);
+//         if (result.done) return;
+//         (result.value as Promise<any>).then(response => {
+//             recursiveIteration(response);
+//         });
+//     }
+//     (iter.next().value as Promise<any>).then(response => {
+//         recursiveIteration(response);
+//     });
+// }
+// // 執行 pingsGenerator 的過程
+// runGenerator(pingsGenerator);
+
+/**最終方式 */
+// console.time('1st Pong');
+// console.time('2nd Pong');
+// console.time('3rd Pong');
+
+// function* pingsGenerator(): any {
+//     try {
+//         const response1 = yield pingRequest(1);
+//         console.log(response1.data);
+//         console.timeEnd('1st Pong');
+//     } catch (err1) {
+//         throw new Error(err1 as string);
+
+//     }
+//     try {
+//         const response2 = yield pingRequest(2);
+//         console.log(response2.data);
+//         console.timeEnd('2nd Pong');
+//     } catch (err2) {
+//         throw new Error(err2 as string);
+
+//     }
+//     try {
+//         const response3 = yield pingRequest(3);
+//         console.log(response3.data);
+//         console.timeEnd('3rd Pong');
+//     } catch (err3) {
+//         throw new Error(err3 as string);
+
+//     }
+// }
+// // 建立一個 ping-request 的迭代器
+// let pingsIter = pingsGenerator();
+
+// // 縮寫的命名代表 Pong-Response-Promise 物件
+// type PRP = Promise<PongResponse>;
+
+// // 第一輪迭代 —— 啟動迭代器
+// (pingsIter.next().value as PRP).then(response1 => {
+//     console.log('Response 1 received');
+
+//     // 將 response1 傳入 next 並進行第二輪迭代
+//     (pingsIter.next(response1).value as PRP).then(response2 => {
+//         console.log('Response 2 received');
+
+//         // 將 response2 傳入 next 並進行第二輪迭代
+//         (pingsIter.next(response2).value as PRP).then(response3 => {
+//             console.log('Response 3 received');
+
+//             // 將 response3 傳入 next 並進行最後一輪迭代
+//             pingsIter.next(response3);
+//         });
+//     });
+// });
+
+/**Async-Await 表現形式 */
+async function pingsAsyncFunc() {
+    try {
+        const response1 = await pingRequest(1);
+        console.log(response1.data);
+        console.timeEnd('1st Pong');
+    } catch (err1) {
+        throw new Error(err1 as string);
+
+    }
+    try {
+        const response2 = await pingRequest(2);
+        console.log(response2.data);
+        console.timeEnd('2nd Pong');
+    } catch (err2) {
+        throw new Error(err2 as string);
+
+    }
+    try {
+        const response3 = await pingRequest(3);
+        console.log(response3.data);
+        console.timeEnd('3rd Pong');
+    } catch (err3) {
+        throw new Error(err3 as string);
+
+    }
+}
+// 執行 Async Function
+// console.log('Using Async Function:');
+
+// console.time('1st Pong');
+// console.time('2nd Pong');
+// console.time('3rd Pong');
+
+// // 呼叫 Asunc Function 想成在處理 Promise 物件
+
+// pingsAsyncFunc()
+//     .then(() => {
+//         console.log('End Request Process !');
+//     });
+
+// 就算是回傳普通值，它一樣會使用 Promise 包起來
+async function returnSomething() {
+    return 42;
+}
+
+function delayAfter<T>(
+    milliseconds: number, value: T
+): Promise<T> {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(value);
+        }, milliseconds);
+    });
+}
+async function example1() {
+    const message = await delayAfter(3000, 'Hello world');
+    console.log("message =", message);
+}
+// example1().then(() => {
+//     console.log('End example1 Process !');
+// });
+async function example2() {
+    const message = await 'Hello world exampl2';
+    console.log("message =", message);
+}
+// example2().then(() => {
+//     console.log('End example2 Process !');
+// });
+
+async function example3() {
+    const result = await Promise.race([
+        delayAfter(5000, 'Hello world'),
+        delayAfter(3000, 123),
+        delayAfter(4000, true),
+    ]);
+    console.log("result=", result);
+}
+// example3().then(() => {
+//     console.log('End example3 Process !');
+// });
+
+async function exampl4() {
+    const result = await Promise.all([
+        delayAfter(5000, 'Hello world'),
+        delayAfter(3000, 123),
+        delayAfter(4000, true),
+    ]);
+    console.log("result=", result);
+}
+// exampl4().then(() => {
+//     console.log('End exampl4 Process !');
+// });
+
+// async function example5_1() {
+//     return await Promise.resolve(123);
+// }
+// async function example5_2() {
+//     return Promise.resolve(123);
+// }
+
+// example5_1().then((res) => {
+//     console.log(res);
+//     console.log('End example5_1 Process !');
+// });
+// example5_2().then((res) => {
+//     console.log(res);
+//     console.log('End example5_2 Process !');
+// });
+
+async function example6_1() {
+    console.log('First async function called.');
+    return await delayAfter(1000, 'Hello!');
+}
+
+async function example6_2() {
+    const result = await example6_1();
+    console.log('Second async function called.');
+    await delayAfter(2000, 'example6_2');
+    return result;
+}
+
+example6_2().then(console.log);
+
+//#endregion Day 50
